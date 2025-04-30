0d245f5d8e2ce54f734c7ec4410245f593377894
Mauro Lisena <maurolisena@gmail.com>
2025-04-30 01:09:44 -0300

    Add getInventoryList endpoint

--- src/main/java/com/mlisena/product/client/InventoryClient.java
+++ src/main/java/com/mlisena/product/client/InventoryClient.java
@@ -6,6 +6,10 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient(
         name = "INVENTORY-SERVICE",
@@ -15,5 +19,8 @@ public interface InventoryClient {
 
     @GetMapping("/api/inventory/{skuCode}")
     Inventory getInventory(@PathVariable @NotBlank String skuCode);
+
+    @PostMapping("/api/inventory/list")
+    List<Inventory> getInventories(@RequestBody @NotBlank List<String> skuCodeList);
 
 }
