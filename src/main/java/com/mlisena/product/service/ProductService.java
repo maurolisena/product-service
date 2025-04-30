5791848f5e3b0c0d3b6c52aedf01ecdf6340fdec
Mauro Lisena <maurolisena@gmail.com>
2025-04-30 01:10:46 -0300

    implementing redis and cache methods to get product stock

--- src/main/java/com/mlisena/product/service/ProductService.java
+++ src/main/java/com/mlisena/product/service/ProductService.java
@@ -5,10 +5,8 @@ import com.mlisena.product.dto.payload.CreateInventoryEvent;
 import com.mlisena.product.dto.request.product.CreateProductRequest;
 import com.mlisena.product.dto.request.product.ProductFilterRequest;
 import com.mlisena.product.dto.request.product.UpdateProductRequest;
-import com.mlisena.product.dto.response.product.ProductListResponse;
 import com.mlisena.product.dto.response.product.ProductResponse;
 import com.mlisena.product.entity.Product;
-import com.mlisena.product.exception.product.ProductNotFoundException;
 import com.mlisena.product.repository.ProductRepository;
 import com.mlisena.product.service.external.InventoryService;
 import com.mlisena.product.service.kafka.KafkaProducerService;
@@ -16,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -45,7 +45,7 @@ public class ProductService {
         log.info("Getting Product with id: {}", id);
         Product product = productRepository.findById(id);
         log.info("Product found with id: {}", id);
-        int stock = inventoryService.productStock(product.getCode());
+        int stock = inventoryService.getProductStock(product.getCode());
         return ProductMapper.toResponse(product, stock);
     }
 
@@ -53,8 +53,15 @@ public class ProductService {
         log.info("Searching Products with filter: {}", request);
         Page<Product> productPage = productRepository.findByFilters(request);
         log.info("Products found: {}", productPage.getTotalElements());
+
+        log.info("Getting stock for Products");
+        Map<String, Integer> stockMap = inventoryService.getProductStockMap(
+            productPage.getContent().stream()
+                .map(Product::getCode)
+                .toList());
+
         return productPage.map(product -> {
-            int stock = inventoryService.productStock(product.getCode());
+            int stock = stockMap.getOrDefault(product.getCode(), 0);
             return ProductMapper.toResponse(product, stock);
         });
     }
@@ -62,7 +69,8 @@ public class ProductService {
     public ProductResponse updateProduct(String id, UpdateProductRequest request) {
         log.info("Updating Product with id: {}", id);
         Product product = productRepository.updateById(id, request);
-        return ProductMapper.toResponse(product, 1);
+        int stock = inventoryService.getProductStock(product.getCode());
+        return ProductMapper.toResponse(product, stock);
     }
 
     public void deleteProduct(String id) {
--- src/main/java/com/mlisena/product/service/external/InventoryService.java
+++ src/main/java/com/mlisena/product/service/external/InventoryService.java
@@ -2,9 +2,13 @@ package com.mlisena.product.service.external;
 
 import com.mlisena.product.client.InventoryClient;
 import com.mlisena.product.dto.response.inventory.Inventory;
+import com.mlisena.product.service.redis.InventoryRedisHelper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +16,43 @@ import org.springframework.stereotype.Service;
 public class InventoryService {
 
     private final InventoryClient inventoryClient;
+    private final InventoryRedisHelper inventoryRedisHelper;
 
-    public int productStock(String skuCode) {
+    public int getProductStock(String skuCode) {
         log.info("Getting stock for SKU code: {}", skuCode);
+        Integer cachedStock = inventoryRedisHelper.getCachedInventoryStock(skuCode);
+
+        if (cachedStock != null) {
+            return cachedStock;
+        }
+
         Inventory inventory = inventoryClient.getInventory(skuCode);
         log.info("Stock for Product with SKU code {}: {}", skuCode, inventory);
+
+        inventoryRedisHelper.cacheProductStock(skuCode, inventory.quantity());
         return inventory.quantity();
+    }
+
+    public Map<String, Integer> getProductStockMap(List<String> skuCodes) {
+        Map<String, Integer> cachedStock = inventoryRedisHelper.getCachedInventoryStockList(skuCodes);
+
+        List<String> missingSkuCodes = skuCodes.stream()
+                .filter(skuCode -> !cachedStock.containsKey(skuCode))
+                .toList();
+
+        if (missingSkuCodes.isEmpty()) {
+            log.info("All SKU codes found in cache");
+            return cachedStock;
+        }
+
+        log.info("Getting missing SKU codes in cache");
+        List<Inventory> inventories = inventoryClient.getInventories(missingSkuCodes);
+
+        inventories.forEach(inventory -> {
+            inventoryRedisHelper.cacheProductStock(inventory.skuCode(), inventory.quantity());
+            cachedStock.put(inventory.skuCode(), inventory.quantity());
+        });
+
+        return cachedStock;
     }
 }
