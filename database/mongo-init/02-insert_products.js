679e923cfa24297c27b3c96d88a24bce02b0ae31
Mauro Lisena <maurolisena@gmail.com>
2025-05-24 13:01:38 -0300

    Revert "Unified scripts to resolve execution moment issue"
    This reverts commit 56e6ca6e07abec8ff4181cb3686980cfefed50e7.

--- database/mongo-init/02-insert_products.js
+++ database/mongo-init/02-insert_products.js
@@ -1,15 +1,5 @@
 db = db.getSiblingDB('product_db');
 
-db.createUser({
-    user: 'mlisena',
-    pwd: 'A8424628',
-    roles: [
-        { role: 'readWrite', db: 'product_db' }
-    ]
-});
-
-db.auth('mlisena', 'A8424628');
-
 db.products.insertMany([
     {
         name: "Filtro de aceite",
