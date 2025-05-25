b7caefa5b6987852132c130b4acec5179517dec6
Mauro Lisena <maurolisena@gmail.com>
2025-05-25 01:44:43 -0300

    Resolve pom dependency issues and new MongockConfig

--- src/main/java/com/mlisena/product/configuration/mongo/MongockConfig.java
+++ src/main/java/com/mlisena/product/configuration/mongo/MongockConfig.java
@@ -1,8 +1,9 @@
-package com.mlisena.product.configuration.mongo.migrations;
+package com.mlisena.product.configuration.mongo;
 
-import io.mongock.driver.mongodb.springdata.v3.SpringDataMongoV3Driver;
+import io.mongock.driver.mongodb.springdata.v4.SpringDataMongoV4Driver;
 import io.mongock.runner.springboot.MongockSpringboot;
 import io.mongock.runner.springboot.base.MongockInitializingBeanRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -11,11 +12,16 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 public class MongockConfig {
 
     @Bean
-    public MongockInitializingBeanRunner mongockInitializingBeanRunner(MongoTemplate mongoTemplate) {
-        SpringDataMongoV3Driver driver = SpringDataMongoV3Driver.withDefaultLock(mongoTemplate);
+    public MongockInitializingBeanRunner mongockInitializingBeanRunner(
+        ApplicationContext springContext,
+        MongoTemplate mongoTemplate
+    ) {
+
+        SpringDataMongoV4Driver driver = SpringDataMongoV4Driver.withDefaultLock(mongoTemplate);
 
         return MongockSpringboot.builder()
                 .setDriver(driver)
+                .setSpringContext(springContext)
                 .addChangeLogsScanPackage("com.mlisena.product.configuration.mongo.migrations")
                 .buildInitializingBeanRunner();
     }
