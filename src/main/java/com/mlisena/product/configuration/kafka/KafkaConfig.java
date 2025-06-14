dcc2643ea1699edce9b37289487c4428be55525f
Mauro Lisena <maurolisena@gmail.com>
2025-06-14 01:36:48 -0300

    Creating KafkaConfig class

---
+++ src/main/java/com/mlisena/product/configuration/kafka/KafkaConfig.java
@@ -0,0 +1,8 @@
+package com.mlisena.product.configuration.kafka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig { }
