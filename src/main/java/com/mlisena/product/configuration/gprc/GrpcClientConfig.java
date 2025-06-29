ed97ac73186b827ed68b6f8cbfdeaff5b88621a4
Mauro Lisena <maurolisena@gmail.com>
2025-06-29 14:54:39 -0300

    Remove grpc profile configuration

--- src/main/java/com/mlisena/product/configuration/gprc/GrpcClientConfig.java
+++ src/main/java/com/mlisena/product/configuration/gprc/GrpcClientConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
 public class GrpcClientConfig {
 
     @Bean
