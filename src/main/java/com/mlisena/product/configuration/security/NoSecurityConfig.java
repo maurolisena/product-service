cd0d9bca56a9f8cf808690f0e6c01dd23c01ed8d
Mauro Lisena <maurolisena@gmail.com>
2025-06-29 13:25:23 -0300

    Implement profile security config

---
+++ src/main/java/com/mlisena/product/configuration/security/NoSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.mlisena.product.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("local-dev")
+public class NoSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authz -> authz
+                    .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+}
